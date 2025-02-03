af593793e871483c76c83d6c5256a4b8b6de171f
Kartikeya Duvvuri <142835117+kduvvuri1@users.noreply.github.com>
2025-02-02 22:48:32 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Initialize AOS (Animate on Scroll)
+AOS.init({
+    duration: 1000, // Animation duration in ms
+    easing: 'ease', // Animation easing
+    once: true // Animations should happen only once
+});
