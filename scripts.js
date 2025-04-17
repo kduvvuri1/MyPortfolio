95d8ba82c48e3d9a8ad5338f033d859674e9aec9
Kartikeya Duvvuri <142835117+kduvvuri1@users.noreply.github.com>
2025-04-16 22:01:12 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-// Initialize AOS (Animate on Scroll)
-AOS.init({
-    duration: 1000, // Animation duration in ms
-    easing: 'ease', // Animation easing
-    once: true // Animations should happen only once
-});
